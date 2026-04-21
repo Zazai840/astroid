0e54962159d73a9c1dd9ebdbaebcfaf8b80f6e65
Abdulaziz Zazai <zazai@Abdulazizs-MacBook-Air.local>
2026-04-21 15:23:02 +0100

    feat: Large asteroids split into smaller ones when hit

--- asteroids.py
+++ asteroids.py
@@ -1,7 +1,8 @@
 from circleshape import CircleShape
 from constants import *
 import pygame
-
+from logger import log_event
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -12,6 +13,22 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position = self.position + self.velocity * dt
+    
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            angle = random.uniform(20, 50)
+            new_ast1_velocity = self.velocity.rotate(angle)
+            new_ast2_velocity = self.velocity.rotate(-angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            ast1 = Asteroid(self.position.x, self.position.y, new_radius)
+            ast2 = Asteroid(self.position.x, self.position.y, new_radius)
+            ast1.velocity = new_ast1_velocity * 1.2
+            ast2.velocity = new_ast2_velocity * 1.2
+
         
     
 
